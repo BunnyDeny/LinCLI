133720c37ba6b5a5d54110309b3402d06248d938
兔兔不给贴贴 <guoy55448@gmail.com>
2026-04-05 23:38:44 +0800

    1

--- cli/Inc/cli_io.h
+++ cli/Inc/cli_io.h
@@ -4,6 +4,58 @@
 #include "tVector.h"
 
 #define CLI_IO_SIZE 128
+#define COLOR_TERMINAL_EN 1
+
+#if COLOR_TERMINAL_EN
+#define COLOR_NONE "\033[0m"
+#define COLOR_RED "\033[31m"
+#define COLOR_GREEN "\033[32m"
+#define COLOR_YELLOW "\033[33m"
+#define COLOR_BLUE "\033[34m"
+#define COLOR_MAGENTA "\033[35m"
+#define COLOR_CYAN "\033[36m"
+#define COLOR_WHITE "\033[37m"
+#define COLOR_BOLD "\033[1m"
+#define COLOR_RAINBOW_1 "\033[38;5;196m" /* 红 */
+#define COLOR_RAINBOW_2 "\033[38;5;208m" /* 橙红 */
+#define COLOR_RAINBOW_3 "\033[38;5;214m" /* 橙 */
+#define COLOR_RAINBOW_4 "\033[38;5;226m" /* 黄 */
+#define COLOR_RAINBOW_5 "\033[38;5;046m" /* 绿 */
+#define COLOR_RAINBOW_6 "\033[38;5;051m" /* 青 */
+#define COLOR_RAINBOW_7 "\033[38;5;033m" /* 蓝 */
+#define COLOR_RAINBOW_8 "\033[38;5;129m" /* 紫 */
+#define COLOR_RAINBOW_9 "\033[38;5;201m" /* 洋红 */
+#else
+#define COLOR_NONE
+#define COLOR_RED
+#define COLOR_GREEN
+#define COLOR_YELLOW
+#define COLOR_BLUE
+#define COLOR_MAGENTA
+#define COLOR_CYAN
+#define COLOR_WHITE
+#define COLOR_BOLD
+#define COLOR_RAINBOW_1
+#define COLOR_RAINBOW_2
+#define COLOR_RAINBOW_3
+#define COLOR_RAINBOW_4
+#define COLOR_RAINBOW_5
+#define COLOR_RAINBOW_6
+#define COLOR_RAINBOW_7
+#define COLOR_RAINBOW_8
+#define COLOR_RAINBOW_9
+#endif
+
+#define KERN_EMERG "0"
+#define KERN_ALERT "1"
+#define KERN_CRIT "2"
+#define KERN_ERR "3"
+#define KERN_WARNING "4"
+#define KERN_NOTICE "5"
+#define KERN_INFO "6"
+#define KERN_DEBUG "7"
+#define KERN_DEFAULT ""
+#define KERN_CONT "c"
 
 struct cli_io {
 	struct vector in;
