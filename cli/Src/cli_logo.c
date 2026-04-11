f68ebbc18daa983f6e523f28b0836d0615a08b49
兔兔不给贴贴 <guoy55448@gmail.com>
2026-04-12 00:54:44 +0800

    logo

---
+++ cli/Src/cli_logo.c
@@ -0,0 +1,24 @@
+#include "cli_io.h"
+#include "init_d.h"
+
+void pr_logo(void *arg)
+{
+	cli_printk(
+		"         ____                                     _____   _        _____ \n");
+	cli_printk(
+		"        |  _ \\                                   / ____| | |      |_   _|\n");
+	cli_printk(
+		"        | |_) |  _   _   _ __    _ __    _   _  | |      | |        | |  \n");
+	cli_printk(
+		"        |  _ <  | | | | | '_ \\  | '_ \\  | | | | | |      | |        | |  \n");
+	cli_printk(
+		"        | |_) | | |_| | | | | | | | | | | |_| | | |____  | |____   _| |_ \n");
+	cli_printk(
+		"        |____/   \\__,_| |_| |_| |_| |_|  \\__, |  \\_____| |______| |_____|\n");
+	cli_printk(
+		"                                          __/ |                          \n");
+	cli_printk(
+		"                                         |___/                           \n");
+}
+
+_EXPORT_INIT_SYMBOL(logo, NULL, pr_logo);
