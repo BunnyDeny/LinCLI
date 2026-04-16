2133bba55a5f32265d7152f62a75ac5e57892e22
rabbit <2039880031@qq.com>
2026-04-16 17:56:13 +0800

    1

--- init/main.c
+++ init/main.c
@@ -1,3 +1,21 @@
+/*
+ * LinCLI - A lightweight C command-line interaction framework for embedded/MCU.
+ * Copyright (C) 2026  bunnydeny <guoy55448@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
@@ -50,6 +68,12 @@ int main()
 	tcgetattr(STDIN_FILENO, &t);
 	t.c_lflag &= ~(ICANON | ECHO); // 禁用规范模式，回显
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &t);
+
+	printf("LinCLI  Copyright (C) 2026  bunnydeny\n");
+	printf("This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.\n");
+	printf("This is free software, and you are welcome to redistribute it\n");
+	printf("under certain conditions; type `show c' for details.\n\n");
+
 	if (pthread_create(&cli_in_thread, NULL, cli_in_entry, NULL)) {
 		fprintf(stderr, "创建线程 cli_in_thread 失败\n");
 		return 1;
