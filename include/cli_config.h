b6fced340812ddc5252bde2f05e8619e3cc8ab65
兔兔不给贴贴 <guoy55448@gmail.com>
2026-05-04 01:06:26 +0800

    chore: 升级版本号至 v1.9.0，准备发布

--- include/cli_config.h
+++ include/cli_config.h
@@ -52,8 +52,8 @@
 
 /* 版本号 */
 #define CLI_VERSION_MAJOR 1
-#define CLI_VERSION_MINOR 8
-#define CLI_VERSION_PATCH 27
+#define CLI_VERSION_MINOR 9
+#define CLI_VERSION_PATCH 0
 
 /* 内存池配置 */
 #define CLI_MPOOL_COUNT 6
