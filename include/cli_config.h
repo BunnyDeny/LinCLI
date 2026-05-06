61fec27251256e5299ea573271321998cde83072
rabbit <2039880031@qq.com>
2026-05-06 16:25:58 +0800

    chore: 升级版本号至 v1.11.0，准备发布

--- include/cli_config.h
+++ include/cli_config.h
@@ -65,8 +65,8 @@
 
 /* 版本号 */
 #define CLI_VERSION_MAJOR 1
-#define CLI_VERSION_MINOR 10
-#define CLI_VERSION_PATCH 2
+#define CLI_VERSION_MINOR 11
+#define CLI_VERSION_PATCH 0
 
 /* 内存池配置 */
 #define CLI_MPOOL_COUNT 6
