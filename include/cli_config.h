d214b87b5eaee4fcca1b6dde2834445fd7a1ef21
rabbit <2039880031@qq.com>
2026-05-11 10:20:29 +0800

    chore: 升级版本号至 v1.15.0，准备发布

--- include/cli_config.h
+++ include/cli_config.h
@@ -30,8 +30,8 @@
  * 以下宏与 Kconfig 无关，始终保留。
  * ========================================================== */
 #define CLI_VERSION_MAJOR 1
-#define CLI_VERSION_MINOR 14
-#define CLI_VERSION_PATCH 9
+#define CLI_VERSION_MINOR 15
+#define CLI_VERSION_PATCH 0
 
 #if CLI_MPOOL_COUNT > 32
 #error "CLI_MPOOL_COUNT must not exceed 32"
